307fb8b601712cd2b741f4b94fe125183102e696
Billy Lee <codingbillylee@gmail.com>
2023-05-20 17:44:57 -0700

    Add all tests for order class

--- VendorOrder.Test/ModelTests/OrderTest.cs
+++ VendorOrder.Test/ModelTests/OrderTest.cs
@@ -29,5 +29,25 @@ namespace VendorOrder.Test
       Assert.AreEqual("x", order.Date);
       Assert.AreEqual(1, order.Price);
     }
+
+    [TestMethod]
+    public void GetAll_GetsAllOrders_OrderList()
+    {
+      Order order1 = new Order("x", "x", "x", 1);
+      Order order2 = new Order("x", "x", "x", 2);
+      List<Order> listReturn = new List<Order> {order1, order2};
+
+      List<Order> list = Order.GetAll();
+      CollectionAssert.AreEqual(listReturn, list);
+    }
+
+    [TestMethod]
+    public void FindId_FindsIdOfObject_Order()
+    {
+      Order order1 = new Order("x", "x", "x", 1);
+      Order order2 = new Order("x", "x", "x", 2);
+      Order find = Order.FindId(1);
+      Assert.AreEqual(order1, find);
+    }
   }
 }
